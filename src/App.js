e0460ef00a78dc76240416fe23feae1e6c57e652
Nazar <nazzikmart@gmail.com>
2023-07-05 21:27:21 +0300

    COMMIT 05/07/2023

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import ClassComponent from "./ClassComponent";
+import Country from "./components/Country";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <ClassComponent />
+      <Country />
+    </div>
+
+    // React.createElement("h1", null, "Hello world")
+  );
+}
+
+export default App;
---
+++ src/components/Country.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Country = () => {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Ukraine</h1>
+    </div>
+  );
+};
+
+export default Country;
